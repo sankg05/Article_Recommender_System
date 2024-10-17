8652d3c92a13ec6b56551a61d00b9f7f5a7430d5
sankg05 <sanikagadkari05@gmail.com>
2024-10-17 23:25:13 +0530

    Committing base project files

---
+++ blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Posts, Category
+# Register your models here.
+
+admin.site.register(Posts)
+admin.site.register(Category)
+
+
---
+++ users/forms.py
@@ -0,0 +1,27 @@
+#for adding fields to the form
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
+
+class UserRegisterForm(UserCreationForm):
+    email=forms.EmailField()
+    contact_number=forms.CharField()
+    
+    class Meta:
+        model=User
+        fields=['username', 'email', 'password1', 'password2']
+        
+class UserUpdateForm(forms.ModelForm):
+    email=forms.EmailField()
+    
+    class Meta:
+        model=User
+        fields=['username', 'email']
+    
+class ProfileUpdateForm(forms.ModelForm):
+    contact_number=forms.CharField()
+
+    class Meta:
+        model=Profile
+        fields=['contact_number', 'image']
